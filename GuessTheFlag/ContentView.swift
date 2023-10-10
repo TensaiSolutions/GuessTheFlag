48cde09355b94c18b28fabe5a87e8c93c1b1fa75
Tensai Solutions <sidellp@gmail.com>
2023-10-10 16:14:09 -0400

    Finished Challenges

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var userScore = 0
+    @State private var numberOfQuestions = 0
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     var body: some View {
@@ -27,7 +29,7 @@ struct ContentView: View {
                     .foregroundColor(.white)
                 Spacer()
                 Spacer()
-                Text("Score: ???")
+                Text("Score: \(userScore)")
                     .foregroundColor(.white)
                     .font(.title.bold())
                 Spacer()
@@ -59,25 +61,42 @@ struct ContentView: View {
             .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue", action: askQuestion)
+            if (numberOfQuestions < 8) {
+                Button("Continue", action: askQuestion)
+            } else {
+                Button("Restart", action: restart)
+            }
         } message: {
-            Text("Your score is ???")
+            if (numberOfQuestions < 8) {
+                Text("Your score is \(userScore)")
+            } else {
+                Text("Your final score was \(userScore)")
+            }
         }
     }
 
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            userScore += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong that is the flag of \(countries[number])"
+            userScore -= 1
         }
 
+        numberOfQuestions += 1
         showingScore = true
     }
 
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+
+    func restart() {
+        userScore = 0
+        numberOfQuestions = 0
+        askQuestion()
     }
 }
 
