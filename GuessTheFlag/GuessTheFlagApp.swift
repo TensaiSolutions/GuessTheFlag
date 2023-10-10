87ea7f7c738329125d598a38b0eb210e2a69ec6e
Tensai Solutions <sidellp@gmail.com>
2023-10-10 12:58:45 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,18 @@
+//
+// GuessTheFlagApp.swift
+// GuessTheFlag
+//
+// Created by Tensai Solutions on 10/10/23
+// Copyright © 2023 Tensai Solutions. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
